e8b062cfdd617db0b2fd7678ae9135c1b3d4c309
KESAVARAJJ <kesavarajj04@gmail.com>
2024-09-13 09:44:59 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Hero from "./Pages/Hero";
+import Section from "./Pages/Section";
+import Section2 from "./Pages/Section2";
+function App() {
+  return (
+    <>
+      <Hero />
+      <Section />
+      <Section2 />
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/src/Pages/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+        <img width="1500%"src="https://startuptn.in/images/Banner/Thiruvizha%20Web%20Banner%20(5).png" alt="logo" />
+    </div>
+  )
+}
+
